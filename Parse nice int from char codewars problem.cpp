c46da2191bbb0a27dd0940c5f6e44dfb6ba2931a
Axat Tiwari <axattiw@gmail.com>
2022-08-04 12:05:44 +0530

    codewar kata using stoi function with minimal code

--- Parse nice int from char codewars problem.cpp
+++ Parse nice int from char codewars problem.cpp
@@ -3,10 +3,5 @@
 
 	int get_age(const std::string & she_said)
 	{
-
-		std::string intPortion{ she_said.substr(0,1) };
-		int ageInInteger{ stoi(intPortion,nullptr,10) };
-		return ageInInteger;
-
-
+		return stoi(she_said);
 	}
