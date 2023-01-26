3d79c5466f97e70821b46398a09824810d8d83db
Axactt <axattiw@gmail.com>
2023-01-26 10:50:18 +0530

    A StaticArray template class, whcih is non-type non-template parameter(being certaiy a constexpr).

---
+++ StaticArray Non-type parameters.cpp
@@ -0,0 +1,43 @@
+// non-dynamic(static) array class using both type parameter and non-type parameter
+#include<iostream>
+template<typename T, int size>// size is an integeral non-type parameter
+class StaticArray // std::array functionality recreated
+{
+private:
+	// The non-type parameter controls the size of the array
+	T m_array[size] {};
+public:
+	T* getArray();
+	T& operator[]( int index )
+	{
+		return m_array[index];
+	}
+};
+//Showing how a function for a class with a non-type parameter is defined outside of the class
+template<typename T,int size>
+T* StaticArray<T, size>::getArray()
+{
+	return m_array;
+}
+
+int main()
+{
+// Declare an integer array with room for 12 integers
+	StaticArray<int, 12> intArray;
+	//Fill it up in order, then print it backwards
+	for (int count { 0 }; count < 12; ++count)
+		intArray[count] = count;
+
+	for (int count { 11 }; count >= 0; --count)
+		std::cout << intArray[count] << ' ';
+	std::cout << '\n';
+   // declare a double buffer with  room for 4 doubles
+	StaticArray<double, 4>doubleArray;
+
+	for (int count { 0 }; count < 4; ++count)
+		doubleArray[count] = 4.4 + 0.1 * count;
+
+	for (int count { 0 }; count < 4; ++count)
+		std::cout << doubleArray[count] << '\n';
+	return 0;
+}
