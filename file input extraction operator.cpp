67aa927e647c724b8cd09ad1f90863d9a63897d2
Axactt <94149911+Axactt@users.noreply.github.com>
2023-04-14 08:18:03 +0530

    File input from a ifstream input file stream object to a std::string object using extraction operator. The extraction opeartor breaks on whitespace, so finally std::getline is used to read from ifstream object to std::string object.

---
+++ file input extraction operator.cpp
@@ -0,0 +1,44 @@
+#include<fstream>
+#include<iostream>
+#include <string>
+
+int main()
+{
+	
+	//!ifstream is used for reading from file buffer
+		// reading from a file called Sample.txt
+	std::ifstream inf{ "Sample.txt" };
+
+	//If we couldn,t open the output file stream for reading
+	if (!inf)
+	{
+		//Print an error and exit
+		std::cerr << "Sample.txt cannot be opened for writing to some string buffer.\n";
+		return 1;
+	}
+
+	/*
+	//While there's still stuff left to read
+	//? ifstream return 0 if we've reached the End of file 
+	//!use this to determine how much to read
+	while (inf)
+	{
+		//!read stuff from the file into a string and print it
+		std::string strInput;
+		//!use extraction opeartor to extract info from file buffer
+		//! extraction opeartor will break on white spaces
+		inf >> strInput;
+		std::cout << strInput << '\n';
+
+	}*/
+	while (inf)
+	{
+		std::string strInput;
+		std::getline( inf, strInput ); //!std::getline so that breaking on white space does not occur
+		std::cout << strInput << '\n';
+	 }
+
+	return 0;
+	//When inf goes out of scope, 
+	//the ifstream destructor will close the file
+}
